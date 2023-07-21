41d59ce3ba869deb8a595b39cb6497872ad48dc6
Mody <mody.ba@telecom-paris.fr>
2023-07-22 01:20:31 +0200

    regroupement de fichers

--- treatment/vlans.py
+++ treatment/vlans.py
@@ -1,6 +1,5 @@
 import  api
-
-import json
+import csv
 import vlans_domains as dm
 
 def update_vlans():
@@ -36,11 +35,30 @@ def update_vlans():
 
         vlan["group"] =vlan.pop ("domainId")
         vlan["vid"] =vlan.pop ("id")
-       
-
-
-
-
 
     return data
+
+
+
+def json_to_csv():
+    csv_file = "data_for_nautobot.csv"
+    data_json = update_vlans()
+    # Vérification des données JSON
+    if not isinstance(data_json, list) or not all(isinstance(item, dict) for item in data_json):
+        raise ValueError("Le paramètre 'data_json' doit être une liste de dictionnaires.")
+
+    # Vérification des noms de colonnes
+    headers = set()
+    for entry in data_json:
+        headers.update(entry.keys())
+
+    # Écrire les données JSON dans le fichier CSV
+    with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer.writeheader()
+        for entry in data_json:
+            writer.writerow(entry)
+
+# Exemple d'utilisation
+json_to_csv()
 
--- treatment/vlans_domains.py
+++ treatment/vlans_domains.py
@@ -1,5 +1,5 @@
 import  api 
-import json
+import csv
 
 def update_vlans_domains_data():
 
@@ -25,5 +25,30 @@ def update_vlans_domains_data():
 
     return data
 
+
+
+def json_to_csv():
+    csv_file = "data_for_nautobot.csv"
+    data_json = update_vlans_domains_data()
+    
+    # Vérification des données JSON
+    if not isinstance(data_json, list) or not all(isinstance(item, dict) for item in data_json):
+        raise ValueError("Le paramètre 'data_json' doit être une liste de dictionnaires.")
+
+    # Vérification des noms de colonnes
+    headers = set()
+    for entry in data_json:
+        headers.update(entry.keys())
+
+    # Écrire les données JSON dans le fichier CSV
+    with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer.writeheader()
+        for entry in data_json:
+            writer.writerow(entry)
+
+
+
+json_to_csv()
 
 
